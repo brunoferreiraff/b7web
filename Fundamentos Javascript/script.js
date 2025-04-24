45cd6eb920331a7a9f1c9b7972c2781f9ef8042a
brunoferreiraff <77634161+brunoferreiraff@users.noreply.github.com>
2025-04-24 17:36:18 -0300

    Organização

---
+++ Fundamentos Javascript/script.js
@@ -0,0 +1,9 @@
+function gravidade(){
+    console.log('A gravidade do planeta é: ');
+    console.log(9.8);
+}
+
+//console.log('Olá, tudo bem?')
+
+
+gravidade(); // Você deve CHAMAR-USAR a função para que ela apareça no log
