436b0a95034b68d7d1e80fb3eee5b73250083979
brunoferreiraff <77634161+brunoferreiraff@users.noreply.github.com>
2025-05-14 23:20:44 -0300

    estudos

--- 003/mandom.js
+++ 003/mandom.js
@@ -1,6 +1,24 @@
 function clicou() {
     const teste = document.querySelector('#teste');
     const ul = teste.querySelector('ul');
-    console.log(ul.outerHTML)
 
+  // ul.children[0].append("(alterado)");
+   
+ 
+
+   /*  ul.append("<li> Item Adicionado<li>");
+
+    let newLi = document.createElement ("li");
+    newLi.innerText = "Item adicionado";
+
+    ul.appendChild(newLi)*/
+   
+    let newUl = document.createElement('ul')
+    
+    for ( let i = 0; i < 5; i++){
+        let newLi = document.createElement('li');
+        newLi.innerHTML = " Item add " + i;
+        newUl.append(newLi);
+    }
+    
 }
