78fd6a471d6f9b1634144163b7671be07008fed5
brunoferreiraff <77634161+brunoferreiraff@users.noreply.github.com>
2025-05-14 23:43:22 -0300

    Estudos

--- 003/mandom.js
+++ 003/mandom.js
@@ -1,5 +1,5 @@
 function clicou() {
-    const teste = document.querySelector('#teste');
+    /*const teste = document.querySelector('#teste');
     const ul = teste.querySelector('ul');
 
   // ul.children[0].append("(alterado)");
@@ -13,12 +13,25 @@ function clicou() {
 
     ul.appendChild(newLi)*/
    
-    let newUl = document.createElement('ul')
+    /*let newUl = document.createElement('ul')
     
     for ( let i = 0; i < 5; i++){
         let newLi = document.createElement('li');
         newLi.innerHTML = " Item add " + i;
         newUl.append(newLi);
-    }
-    
-}
+    }*/
+
+
+   const input = document.querySelector('input');
+   const botao = document.querySelector('.botao');
+
+   if(input.getAttribute('type') === 'text'){
+    input.setAttribute('type', 'password');
+    botao.innerText = 'Ocultar Senha';
+   } else{
+    input.setAttribute('type', 'text');
+    botao.innerText = 'Mostrar Senha';
+   }
+
+
+    }    
